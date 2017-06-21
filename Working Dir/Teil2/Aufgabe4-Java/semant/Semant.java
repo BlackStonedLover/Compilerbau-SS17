65c44d93ae5c1a79978d55a046a76548e11b1788
Konstantin Kühn <konstantin.kuehn@mni.thm.de>
2017-06-21 10:36:59 +0200

    semant push

--- Working Dir/Teil2/Aufgabe4-Java/semant/Semant.java
+++ Working Dir/Teil2/Aufgabe4-Java/semant/Semant.java
@@ -14,19 +14,16 @@ public class Semant {
 
   private boolean showTables;
   static final Type intType = new PrimitiveType("int",VarAllocator,INTBYTESIZE);
-  
+
   public Semant(boolean s) {
     showTables = s;
   }
 
   public Table check(Absyn program) {
     /* generate built-in types */
-    /* setup global symbol table */
-
-
-       
+    /* setup global symbol table */       
     Table globalTable = new TableBuilder().buildSymbolTables(program,showTables);
-/* do semantic checks in 2 passes */
+    /* do semantic checks in 2 passes */
 
 
     new ProcedureBodyChecker().check(program,globalTable);
@@ -35,16 +32,7 @@ public class Semant {
     /* return global symbol table */
     return globalTable;
 
-    /* public table check(Absyn program, boolean showtables){
-    table globalTable = new TableBuilder().buildSymbolTables(program,showTables)
-    new ProcedureBodyChecker().check(program,globalTable)
-    check MainProcedure(globalTable),
-    return globalTable
-    }
-    */
 
-    // check Class(Object,Class<?>,String errorMsg, int lineNo)
-    // check Main Procedure
   }
 
 }
@@ -61,11 +49,11 @@ class TableBuilder{
         program.accept(new TableBuilderVisitor(globalTable));
       return globalTable;
     }
-	
    void  initalizeSymbolTable(){
-     enter Declaration(node.name,new TypeEntry(resultType));
+     
      globalTable.enter(node.name ,new PrimitiveType("int"));
    }
+
 }
 
 private class TableBuilderVisitor extends DoNothingVisitor{
@@ -73,20 +61,21 @@ private class TableBuilderVisitor extends DoNothingVisitor{
   private Type resultType;
   private ParamTypeList paramTypeList;
   
-    public void visit(TypeDec node){
-    node.ty.accept(this);
-    enter Declaration(node.name,new TypeEntry(resultType));
+public void visit(TypeDec node){
+   node.ty.accept(this);
+    if(node.instance.of(IntExp)){
+    paramTypeList.add(new ParamTyp(resultType),node.isRef);
+    SymTable.enter(new VarEntry(resultType,node.isRef));
     }
-	public void visit(ArrayTy node){
-	node.baseTy.accept(this);
-	resultType = new ArrayType(node.size,resultType);
-	}
- 
-	public void visit(NameTy node){
-	Entry e = symTable.getDeclaration(node.name,errorMsg)
-	SemanticCheck.checkClass(e,TypeEntry.class,errorMsg)
-	resultType=(TypeEntry)e type;
-	}
+}
+
+   
+  public void visit(VarDec node){
+  node.ty.accept(this);
+  localTable.enter(new VarEntry(resultType,node.isRef),false);
+  }
+
+
 }
 
 
@@ -94,14 +83,43 @@ private class TableInitializer {
   private Table SymTable;
   private Type resultType;
   private ParamTypeList ParamTypeList;
-  private DecList DecList;
- 
   
   public void initalizeSymbolTable(Table globalTable){
     
   }
 
 }
+
+class CheckVisitor ...{
+Typ resultType;
+public void visit (CallStm node){
+//1. Schritt ist die Prozedur dekleriert Look up
+// Eintrag der Prozedur in der globalTable suchen
+// daraus erhalten wir => paramTypeList + localTable
+
+//2 Listen Abgleichen: paramTypeList | node.args
+// z.B. for-Schleife
+// Für jede Parameterposition:
+//Typ des aktuellen Arguements berechnen
+arg.accept(this); // zu finden danach in resultType
+//Abgleich mit dem Type aus der paramTypeList wenn gleich ok ansonsten fehler
+//Typen vergleichen
+// bei isref = true => Prüfen ob variable übergeben wurde!
+instance.of(VarExp); // checkclass
+// Überprüfen ob die andere Liste .hasNext()? weiter mit Next: Fehler! Abbruch
+/*
+*/
+
+
+//2. ? Schritt schauen ob der Eintrag instance of ProcDec ist oder ob wir es finden
+}
+}
+
+
+
+
+
+
 /*
 class TableBuilder{
 ...
@@ -122,11 +140,21 @@ private class TableBuilderVisitor extends DoNothingVisitor{
   enter Declaration(node.name,new TypeEntry(resultType))
   }
 
+}
+public void visit(ArrayTy node){
+node.baseTy.accept(this);
+resultType = new ArrayType(node.size,resultType);
+}
+public void visit(NameTy node){
+Entry e = symTable.getDeclaration(node.name,errorMsg)
+SemanticCheck.checkClass(e,TypeEntry.class,errorMsg)
+resultType=(TypeEntry)e)type;
+}
+}
 
-/*
+
 ProcedureBodyChecker Overview
-
-DecList : alle Elemente besuchen
+DeclList : alle Elemente besuchen
 
 TypeDec : /////
 
@@ -140,4 +168,5 @@ WhileStmt :
 node.test.accept(this);
 checkType(boolType,"errorMsg",node.row);
 node.body.accept(this);
+
 */
